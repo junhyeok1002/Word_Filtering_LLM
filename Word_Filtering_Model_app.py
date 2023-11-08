d80c9ca33bd8a56fbc4a7ef11faf6505e6eeb702
Jun Hyeok Choi <113586985+junhyeok1002@users.noreply.github.com>
2023-11-08 14:05:35 +0900

    Add files via upload

--- Word_Filtering_Model_app.py
+++ Word_Filtering_Model_app.py
@@ -104,27 +104,45 @@ if User_question != None :
 
 
 else :
-    with st.form("my_form"):
-        image = Image.open('./image/chat_bot_icon.jpg')
-        ex1 = st.chat_message("human", avatar="user")
-        ex1.markdown(f'<p class="Hahmlet_Bold1">User</p>', unsafe_allow_html=True)
-        ex1.write("")
-        ex1.markdown(f'<p class="Hahmlet">EX) 감염 경로를 알 수 없는 확진자를 뭐라고 불러?</p>', unsafe_allow_html=True)
-        ex1.write("\n")
+    if option == '질문→GPT답변→필터링' :
+        with st.form("my_form"):
+            image = Image.open('./image/chat_bot_icon.jpg')
+            ex1 = st.chat_message("human", avatar="user")
+            ex1.markdown(f'<p class="Hahmlet_Bold1">User</p>', unsafe_allow_html=True)
+            ex1.write("")
+            ex1.markdown(f'<p class="Hahmlet">EX) 감염 경로를 알 수 없는 확진자를 뭐라고 불러?</p>', unsafe_allow_html=True)
+            ex1.write("\n")
 
-        ex2 = st.chat_message("human", avatar="ai")
-        ex2.markdown(f'<p class="Hahmlet_Bold2">Chat-GPT</p>', unsafe_allow_html=True)
-        ex2.write("")
-        ex2.markdown(f'<p class="Hahmlet">EX) 깜깜이 확진자라고 합니다!</p>', unsafe_allow_html=True)
-        ex2.write("\n")
+            ex2 = st.chat_message("human", avatar="ai")
+            ex2.markdown(f'<p class="Hahmlet_Bold2">Chat-GPT</p>', unsafe_allow_html=True)
+            ex2.write("")
+            ex2.markdown(f'<p class="Hahmlet">EX) 깜깜이 확진자라고 합니다!</p>', unsafe_allow_html=True)
+            ex2.write("\n")
 
-        ex3 = st.chat_message("user", avatar = image)
-        ex3.markdown(f'<p class="Hahmlet_Bold3">After Filtering</p>', unsafe_allow_html=True)
-        ex3.write("")
-        ex3.markdown(f'<p class="Hahmlet">EX) 시각장애인을 고려하지 못한 표현인데... ㅠㅠ </p>', unsafe_allow_html=True)
-        ex3.write("\n")
+            ex3 = st.chat_message("user", avatar = image)
+            ex3.markdown(f'<p class="Hahmlet_Bold3">After Filtering</p>', unsafe_allow_html=True)
+            ex3.write("")
+            ex3.markdown(f'<p class="Hahmlet">EX) 시각장애인을 고려하지 못한 표현인데... ㅠㅠ </p>', unsafe_allow_html=True)
+            ex3.write("\n")
 
-        st.form_submit_button("아래의 채팅창으로 질문해보세요",use_container_width=True,type = "primary")
+            st.form_submit_button("아래의 채팅창으로 질문해보세요",use_container_width=True,type = "primary")
+    else :
+        with st.form("my_form"):
+            image = Image.open('./image/chat_bot_icon.jpg')
+            ex1 = st.chat_message("human", avatar="user")
+            ex1.markdown(f'<p class="Hahmlet_Bold1">User</p>', unsafe_allow_html=True)
+            ex1.write("")
+            ex1.markdown(f'<p class="Hahmlet">EX) 깜깜이 확진자는 감염경로를 알 수 없는 확진자야!</p>', unsafe_allow_html=True)
+            ex1.write("\n")
+
+            ex3 = st.chat_message("user", avatar=image)
+            ex3.markdown(f'<p class="Hahmlet_Bold3">After Filtering</p>', unsafe_allow_html=True)
+            ex3.write("")
+            ex3.markdown(f'<p class="Hahmlet">EX) 시각장애인을 고려하지 못한 표현인데... ㅠㅠ </p>', unsafe_allow_html=True)
+            ex3.write("\n")
+
+            st.form_submit_button("아래의 채팅창으로 질문해보세요", use_container_width=True, type="primary")
+
 
     for i in range(10):
         st.write("")
